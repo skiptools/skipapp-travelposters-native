baa1d79d470838f4b9fdeb58382ad7dc3bb89f80
Marc Prud'hommeaux <marc@skip.tools>
2024-12-07 10:57:51 -0500

    Add test case

--- travel-posters-model/Tests/TravelPostersModelTests/TravelPostersModelTests.swift
+++ travel-posters-model/Tests/TravelPostersModelTests/TravelPostersModelTests.swift
@@ -1,14 +1,31 @@
 import XCTest
 import Foundation
 import OSLog
+import SkipBridgeKt
 @testable import TravelPostersModel
 
 let logger: Logger = Logger(subsystem: "TravelPostersModel", category: "Tests")
 
 @available(macOS 13, *)
 final class TravelPostersModelTests: XCTestCase {
+    override func setUp() {
+        #if SKIP
+        loadPeerLibrary(packageName: "travel-posters-model", moduleName: "TravelPostersModel")
+        #endif
+    }
+
     func testTravelPostersModel() throws {
         logger.log("running testTravelPostersModel")
-        XCTAssertEqual(1 + 2, 3, "basic test")
+        let allCities = CityManager.shared.allCities
+        let cities: [City]
+        #if SKIP
+        // need to wrap in an Array because kotlincompat mode exposes it as a Kotlin List
+        let citiesList: kotlin.collections.List<travel.posters.model.City> = allCities
+        cities = skip.lib.Array(citiesList)
+        #else
+        cities = allCities
+        #endif
+        XCTAssertEqual(28, cities.count)
+        XCTAssertEqual("Amsterdam", cities.first?.name)
     }
 }
